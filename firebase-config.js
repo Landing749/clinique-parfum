7d4698b002632aaac82914ac28d087e6042eaa72
Landing749 <arhanmeir.obrero@gmail.com>
2025-11-16 18:42:03 +0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,20 @@
+<script>
+/*
+  Firebase Realtime Database config
+  (You already provided these values. Keep this file public.)
+*/
+const firebaseConfig = {
+  apiKey: "AIzaSyB6FAMgqHcccsqce4R5nd8y0YVggc3aHbY",
+  authDomain: "clinique-parfum.firebaseapp.com",
+  databaseURL: "https://clinique-parfum-default-rtdb.firebaseio.com",
+  projectId: "clinique-parfum",
+  storageBucket: "clinique-parfum.firebasestorage.app",
+  messagingSenderId: "569473208892",
+  appId: "1:569473208892:web:f009390a8631fc164ce939",
+  measurementId: "G-XW3R3D64PR"
+};
+
+// Client-side admin password (CHANGE THIS)
+// This is only basic protection. For production use Firebase Auth + DB rules.
+const ADMIN_PASSWORD = "Clinique Parfum";
+</script>
